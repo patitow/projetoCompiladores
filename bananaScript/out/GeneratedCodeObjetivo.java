8a9220d166be9d7157aec3a02b624e68eb9e0df8
João Gabriel Torreiro Gila <92312191+joaogabrieltg@users.noreply.github.com>
2023-09-15 03:37:34 +0000

    codigo alvo corrigido

--- bananaScript/out/GeneratedCodeObjetivo.java
+++ bananaScript/out/GeneratedCodeObjetivo.java
@@ -2,7 +2,6 @@ public class GeneratedCode {
     public static void main(String[] args) {
         main();    
     }
-
     public static int soma(float a , double b ) {
         int d = 1;
         for (int c= 0; c <= 10 ; c++) {
@@ -19,19 +18,20 @@ public class GeneratedCode {
         } 
         while (b > 0 ) {
             b = b - 1;
-        }
+        } 
         try {
             a = b + c;
-        } catch(Exception e) {
+        } catch (Exception e) {
             a = b - c;
         }
+        int y = a + b;
         return a + b;
     }
-    
+
     public static void main() {
         int a = 10;
         int b = 5;
-        int c = a + b;
+        int c = soma(a, b);
+        System.out.println("printo");
     }
-  }
-
+}
